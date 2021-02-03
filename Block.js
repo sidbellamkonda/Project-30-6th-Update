a65f2d44ccf66bf76ff8e61c0accfd662ac598e4
sidbellamkonda <71858026+sidbellamkonda@users.noreply.github.com>
2021-02-03 16:06:44 -0500

    Update Block.js

--- Block.js
+++ Block.js
@@ -18,6 +18,10 @@ class Block{
         if(this.visibility < 0 && this.visibility > -105 && gameState == "launched"){
           score++;
         }
+        
+        if(this.visibility < 0 && this.visibility > -105 && gameState == "onSling2"){
+          score++;
+        }
 
         if(this.visibility < 0 && this.visibility > -52.5 && gameState == "launched2"){
           score++;
